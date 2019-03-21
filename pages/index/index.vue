ee24a8e7db5a4422e188e55706659a564b9f5f42
bakenray <lpqstrong@163.com>
2019-03-21 17:51:15 +0800

    banner carousel

---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="page">
+		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
+			<swiper-item>
+				<image src="../../static/banner/banner-1.jpg" class="carousel"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/banner/banner-2.jpg" class="carousel"></image>
+			</swiper-item>
+		</swiper>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.carousel{
+		width:100%;
+		height:440upx;
+	}
+</style>
