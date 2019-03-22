9ccc51e2a43de3c8ea458872f3f8ca8d1b6619e1
bakenray <lpqstrong@163.com>
2019-03-22 10:11:47 +0800

    发起banner请求图片并渲染

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,34 @@
 <template>
 	<view class="page">
 		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
-			<swiper-item>
-				<image src="../../static/banner/banner-1.jpg" class="carousel"></image>
-			</swiper-item>
-			<swiper-item>
-				<image src="../../static/banner/banner-2.jpg" class="carousel"></image>
+			<swiper-item v-for="image in bannerImages" :key="image.id">
+				<image :src="image.image" class="carousel"></image>
 			</swiper-item>
 		</swiper>
-		
 	</view>
 </template>
-
 <script>
+	import common from "../../common/common.js"
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				bannerImages:[]
 			}
 		},
 		onLoad() {
+		var serverUrl = common.serverUrl
+		uni.request({
+			url: serverUrl +'/index/carousel/list',
+			method:'POST',
+			success: res => {
+				if(res.data.status ===200){
+					this.bannerImages = res.data.data
+				}
+			},
+			fail: () => {},
+			complete: () => {}
+		});
 
 		},
 		methods: {
